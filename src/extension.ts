b074b6d827ce5b8b31f6a3df4ac8babd45b18672
utagawa kiki <utagawakiki@gmail.com>
2020-02-21 09:28:57 +0900

    stub implementation

--- src/extension.ts
+++ src/extension.ts
@@ -10,18 +10,20 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "perl-rename-symbol" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+	let disposable = vscode.languages.registerRenameProvider({ language: 'perl' }, renameProvider);
 
 	context.subscriptions.push(disposable);
 }
 
+const renameProvider = {
+	prepareRename(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.Range> {
+		throw new Error('TODO');
+	},
+
+	provideRenameEdits(document: vscode.TextDocument, position: vscode.Position, newName: string, token: vscode.CancellationToken): vscode.ProviderResult<vscode.WorkspaceEdit> {
+		throw new Error('TODO');
+	}
+};
+
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
