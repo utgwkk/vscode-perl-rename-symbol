89b7914e84b6f435e2342c47cc0b4678dea33bd0
utagawa kiki <utagawakiki@gmail.com>
2020-03-02 08:38:44 +0900

    can extract subroutine name

--- src/extension.ts
+++ src/extension.ts
@@ -10,6 +10,21 @@ async function getTargetFiles(): Promise<string[]> {
 	return [...plFiles, ...pmFiles, ...tFiles];
 }
 
+const subRegex = /sub\s+([_a-zA-Z][_a-zA-Z0-9]*)\s*{/;
+
+function getSubName(document: vscode.TextDocument, position: vscode.Position): string | undefined {
+	for (let lineno = position.line; lineno >= 0; lineno--) {
+		const lineStart = new vscode.Position(lineno, 0);
+		const line = document.getText(new vscode.Range(lineStart, position));
+		const m = line.match(subRegex);
+		if (m === null) {
+			return undefined;
+		}
+		return m[1];
+	}
+	return undefined;
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
