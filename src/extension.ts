2711be6cbe9591ad5422ec5fdd91c8a5a7786240
utagawa kiki <utagawakiki@gmail.com>
2020-03-01 16:59:15 +0900

    activate on Perl file

--- src/extension.ts
+++ src/extension.ts
@@ -5,12 +5,7 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "perl-rename-symbol" is now active!');
-
-	let disposable = vscode.languages.registerRenameProvider({ language: 'perl' }, renameProvider);
+	let disposable = vscode.languages.registerRenameProvider({ scheme: 'file', language: 'perl' }, renameProvider);
 
 	context.subscriptions.push(disposable);
 }
