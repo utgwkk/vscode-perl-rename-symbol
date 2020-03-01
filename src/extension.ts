2c836bd306730c83add111d3a628433f276dedf7
utagawa kiki <utagawakiki@gmail.com>
2020-03-01 23:10:06 +0900

    main implementation

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,57 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as cp from 'child_process';
+
+function getWorkspaceRootDirectory(): string | undefined {
+	const folders = vscode.workspace.workspaceFolders;
+	if (folders === undefined) {
+		return;
+	}
+	const folder = folders[0];
+	if (folder === undefined) {
+		return;
+	}
+	return folder.uri.path;
+}
+
+async function getTargetFiles(): Promise<string[]> {
+	const pmFiles = (await vscode.workspace.findFiles('**/**.pm')).map(url => url.path);
+	const tFiles = (await vscode.workspace.findFiles('**/**.t')).map(url => url.path);
+	return [...pmFiles, ...tFiles];
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.languages.registerRenameProvider({ scheme: 'file', language: 'perl' }, renameProvider);
+	let disposable = vscode.languages.registerRenameProvider(
+		{ scheme: 'file', language: 'perl' }, {
+		prepareRename(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.Range> {
+			const identifierRange = document.getWordRangeAtPosition(position);
+			return identifierRange;
+		},
+
+		provideRenameEdits(document: vscode.TextDocument, position: vscode.Position, newName: string, token: vscode.CancellationToken): vscode.ProviderResult<vscode.WorkspaceEdit> {
+			const identifierRange = document.getWordRangeAtPosition(position);
+			console.log(identifierRange);
+			if (identifierRange === undefined) {
+				return;
+			}
+
+			return new Promise((resolve) => {
+				getTargetFiles().then(targetFiles => {
+					const oldName = document.getText(identifierRange);
+					const prtArgs = ['prt', 'replace_token', oldName, newName, ...targetFiles];
+					const cwd = getWorkspaceRootDirectory();
+					cp.execSync(prtArgs.join(' '), { cwd, encoding: 'utf-8' });
+					resolve(new vscode.WorkspaceEdit());
+				});
+			});
+		}
+	});
 
 	context.subscriptions.push(disposable);
 }
-
-const renameProvider = {
-	prepareRename(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.Range> {
-		throw new Error('TODO');
-	},
-
-	provideRenameEdits(document: vscode.TextDocument, position: vscode.Position, newName: string, token: vscode.CancellationToken): vscode.ProviderResult<vscode.WorkspaceEdit> {
-		throw new Error('TODO');
-	}
-};
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
